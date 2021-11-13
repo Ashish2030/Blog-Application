c345a25e6bd293486bc48b6d7692e1091d34734c
1811981075 <kumarashish79924@gmail.com>
2021-11-13 14:03:08 +0530

    2nd

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  has_one_attached :image
   has_many :comments, dependent: :delete_all
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
